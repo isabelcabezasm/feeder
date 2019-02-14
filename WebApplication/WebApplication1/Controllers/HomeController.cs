802db9061e9a9488f44606c9e093e3ba570565dc
Isabel Cabezas Martín <isacabe@microsoft.com>
2019-02-14 17:19:42 +0100

    add web application again

---
+++ WebApplication/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Let's feed the rabbit.";
+
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult About(int seconds=1)
+        {
+            //ViewBag.Message = "Let's feed the rabbit.";
+            
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
