408278550d19bd177b35bacff420632de428a270
Isabel Cabezas Martín <isacabe@microsoft.com>
2019-03-02 20:11:33 +0100

    add exec to install.sh

--- iot/install.sh
+++ iot/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-if [-z "$1"] echo "Usage $0 [DEVICE_CONNECTION_STRING]" && exit 0;
 
-# need superuser for the gpio library
-[ `whoami` = root ] || exec su -c $0 root
+if [ -z "$1" ]
+then
+ echo "Usage $0 [DEVICE_CONNECTION_STRING]"
+ exit 0
+fi
 
-echo "Executing as su"
+echo "Installing main.js as a daemon with pm2"
+sudo DEVICE_CONNECTION_STRING=$1  pm2 start main.js
+sudo pm2 startup
+sudo pm2 save
 
-DEVICE_CONNECTION_STRING=$1  pm2 start main.js
-pm2 startup
-pm2 save
-
-echo "Done"
