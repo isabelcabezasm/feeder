c98b4dc8303c288a7066c700bd85ceaa5389637d
Isabel Cabezas Martín <isacabe@microsoft.com>
2019-03-02 19:54:57 +0100

    Add install script

---
+++ iot/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [-z "$1"] echo "Usage $0 [DEVICE_CONNECTION_STRING]" && exit 0;
+
+# need superuser for the gpio library
+[ `whoami` = root ] || exec su -c $0 root
+
+DEVICE_CONNECTION_STRING=$1  pm2 start main.js
+pm2 startup
+pm2 save
