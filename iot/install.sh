26b3f1061f319a9a8898717b367348e691eb17a2
Isabel Cabezas Martín <isacabe@microsoft.com>
2019-03-02 19:57:37 +0100

    fix crlf to lf

--- iot/install.sh
+++ iot/install.sh
@@ -4,6 +4,10 @@ if [-z "$1"] echo "Usage $0 [DEVICE_CONNECTION_STRING]" && exit 0;
 # need superuser for the gpio library
 [ `whoami` = root ] || exec su -c $0 root
 
+echo "Executing as su"
+
 DEVICE_CONNECTION_STRING=$1  pm2 start main.js
 pm2 startup
 pm2 save
+
+echo "Done"
